42cfa326bed60b3336bc146436e69119f8333002
JuanBur10 <juanjosebur12@gmail.com>
2026-02-16 17:46:11 -0500

    Subo ejercicios completos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 25;
+        double height = 1.75;
+        bool is_student = true;
+        string name = "Juan José";
+        char grade = 'A';
+
+        Console.WriteLine("La edad es: " + age);
+        Console.WriteLine("La estatura es: " + height);
+        Console.WriteLine("¿Es estudiante?: " + is_student);
+        Console.WriteLine("El nombre es: " + name);
+        Console.WriteLine("La calificación es: " + grade);
+    }
+}
